d86b49d309312140b8aebe9650f250490192c42c
pipiyo <fe.cavieres@gmail.com>
2016-12-07 15:48:32 -0300

    model ususrio y profile picture

--- app/srcApirest/controllers/loginSocket.js
+++ app/srcApirest/controllers/loginSocket.js
@@ -1,5 +1,7 @@
 const jwt = require('jwt-simple')
 const moment = require('moment')
+const User = require('../models/user')
+const Userr = require('../models/user')
 
 module.exports = (io, request) => {
 
@@ -8,6 +10,29 @@ module.exports = (io, request) => {
   .on('connection', (socket) => {
 
     socket.on('login', ( data, callback) => {
+
+User.
+  findOne({ name: data.userName }).
+  exec( (err, user) => {
+    if (err) throw err
+
+    if (user) {
+
+      if (user.password == data.pass) {
+
+
+        callback({ 
+                    token: jwt.encode( { name: `${user.name}`, type: `${user.type}`, expire: `${moment().format('h:mm:ss')}` }, 'xxx'),
+                    name: `${user.name}`, type: `${user.type}`, nombre_completo: `${user.employee.name} ${user.employee.last_name} ${user.employee.second_name}`
+                  })
+      
+
+      } else {
+        callback(false)
+      }
+
+    } else {
+
         request.post({
           uri: process.env.apiLogin,
           form: {
@@ -18,6 +43,69 @@ module.exports = (io, request) => {
           if (error) throw error
             auht = JSON.parse(auht)
           if(auht.on){
+
+
+            let user = new User({ 
+                                  name: auht.name,
+                                  password: data.pass,
+                                  type: auht.type,
+                                  profile_picture: `http://pokeapi.co/media/img/${Math.floor(Math.random() * 718) + 1}.png`,
+                                  employee: {
+                                    rut: auht.employee.rut,
+                                    name: auht.employee.name,
+                                    last_name: auht.employee.last_name,
+                                    second_name: auht.employee.second_name,
+                                    email: auht.employee.email,
+                                    phone: auht.employee.phone,
+                                    mobile: auht.employee.mobile,
+                                    department: auht.employee.department,
+                                    distric: auht.employee.distric,
+                                    nationality: auht.employee.nationality,
+                                    position: auht.employee.position,
+                                    address: auht.employee.address
+                                  }
+                                  })
+
+                  user.save().then( (doc) => {
+
+                    callback({ 
+                                token: jwt.encode( { name: `${doc.name}`, type: `${doc.type}`, expire: `${moment().format('h:mm:ss')}` }, 'xxx'),
+                                name: `${doc.name}`, type: `${doc.type}`, nombre_completo: `${doc.employee.name} ${doc.employee.last_name} ${doc.employee.second_name}`
+                              })
+
+
+                  }, (error) => {
+                    console.log( error )
+                    callback(false)
+                  })
+
+
+          }else{
+
+            callback(false)
+
+          }
+        })
+
+    }
+
+  } )
+
+
+
+
+/*
+        request.post({
+          uri: process.env.apiLogin,
+          form: {
+            user: data.userName,
+            pass: data.pass
+          }
+        }, (error, response, auht) => {
+          if (error) throw error
+            auht = JSON.parse(auht)
+            console.log( auht )
+          if(auht.on){
             
             let token = { token: jwt.encode( { name: `${auht.name}`, type: `${auht.type}`, expire: `${moment().format('h:mm:ss')}` }, 'xxx'), name: `${auht.name}`, type: `${auht.type}`}
 
@@ -26,6 +114,10 @@ module.exports = (io, request) => {
             callback(false)
           }
         })
+*/
+
+
+
     })
 
 /*
---
+++ app/srcApirest/models/user.js
@@ -0,0 +1,36 @@
+let mongoose = require("mongoose")
+
+//let redis = require("redis")
+
+//let pub = redis.createClient()
+
+mongoose.Promise = global.Promise
+
+mongoose.connect("mongodb://localhost/papaya")
+
+let Schema = mongoose.Schema
+
+let userSchema = new Schema({
+	name: String,
+	password: String,
+	type: String,
+	profile_picture: String,
+	employee: {
+		rut: String,
+		name: String,
+		last_name: String,
+		second_name: String,
+		email: String,
+		phone: String,
+		mobile: String,
+		department: String,
+		distric: String,
+		nationality: String,
+		position: String,
+		address: String
+	}
+})
+
+let user = mongoose.model('user', userSchema)
+
+module.exports = user
