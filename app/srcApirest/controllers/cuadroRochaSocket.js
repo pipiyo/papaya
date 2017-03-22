00bd7ffb6b7d536e679d5b1379b0dc2c96f5bdb3
pipiyo <fe.cavieres@gmail.com>
2017-03-22 18:39:15 -0300

    hols

--- app/srcApirest/controllers/cuadroRochaSocket.js
+++ app/srcApirest/controllers/cuadroRochaSocket.js
@@ -1,5 +1,6 @@
 const pool = require('../models/connection')
 const cuadroRochaSearch = require('../searchs/cuadroRochaSearch')
+const _ = require('lodash')
 
 module.exports = (io) => {
 
@@ -23,8 +24,70 @@ module.exports = (io) => {
 
 	  })//on getRochas
 
-	  socket.on('sillas', (color) => {
+	  socket.on('sillas', (lista, callback) => {
 
+
+let insert = `INSERT INTO producto (CODIGO_PRODUCTO, DESCRIPCION, CATEGORIA, CODIGO_GENERICO, FAMILIA, TEMPORADA, 
+						GESTION, DIMENSION, RELACION, TIPO, TERMINO,
+						RUTA, RUTA1, RUTA2, CAD_2D, CAD_3D,
+						POSICION, CUERPO, FRENTE, CANTO, TRASCARA) VALUES ` 
+
+		_.forEach(lista, (v, k) => {
+
+			insert += `('${v.cod}','${v.des}','${v.cat}','','generico','2',
+					   '${v.pais}','${v.proveedor}','${v.modelo}','${v.mecanismo}','${v.respaldo}',
+					   '${v.cod}_img.jpg','${v.cod}_img_1.jpg','${v.cod}_img_2.jpg','${v.cod}.dwg','${v.cod}.dwg',`
+
+
+			insert += ( Array.isArray(v.asiento) ) ? `'${v.asiento[0]}','${v.asiento[1]}',` : `'${v.asiento}','0',`
+
+			insert += (Array.isArray(v.respaldocolor)) ? `'${v.respaldocolor[0]}','${v.respaldocolor[1]}',` : `'${v.respaldocolor}','0',`
+
+			insert += `'${v.estructura}'),`
+
+
+
+
+			_.forEach(v.hijos, (vv, kk) => {
+
+
+				insert += `('${vv.cod}','${vv.des}','${vv.cat}','','silla','2',
+						   '${v.pais}','${v.proveedor}','${v.modelo}','${v.mecanismo}','${v.respaldo}',
+						   '${vv.cod}_img.jpg','${vv.cod}_img_1.jpg','${vv.cod}_img_2.jpg','${vv.cod}.dwg','${vv.cod}.dwg',
+						   '${vv.asiento}','0','${vv.respaldocolor}','0','${vv.estructura}'),`
+
+			})
+
+		})
+
+insert = insert.substring(0, insert.length-1)
+
+callback( insert )
+
+/*
+    pool.getConnection( (err, connection) => {
+          connection.query(`INSERT INTO producto (CODIGO_PRODUCTO, DESCRIPCION, CATEGORIA, CODIGO_GENERICO, FAMILIA, TEMPORADA, 
+						GESTION, DIMENSION, RELACION, TIPO, TERMINO,
+						POSICION, CUERPO, FRENTE, CANTO, TRASCARA,
+						RUTA, RUTA1, RUTA2, CAD_2D, CAD_3D,
+						POSICION, CUERPO, FRENTE, CANTO, TRASCARA) VALUES ${insert}`, (err, rows, fields) => {
+            connection.release()
+            if (err) console.log( err ) 
+            callback( rows )
+          })
+    })
+*/
+
+	  })//on getRochas
+
+
+ })//connection socket
+
+}//module.exports
+
+
+
+/*
 	      pool.getConnection( (err, connection) => {
 	          connection.query(query+query1+query2, (err, rows, fields) => {
 	              connection.release()
@@ -34,10 +97,4 @@ module.exports = (io) => {
 	                reject( console.log('Error ' + err) )
 	          }) 
 	      })
-
-	  })//on getRochas
-
-
- })//connection socket
-
-}//module.exports
+*/
