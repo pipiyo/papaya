ec4ad9d4335a2934df90980fe8a0c7ede3edfaf4
pipiyo <fe.cavieres@gmail.com>
2017-04-23 15:23:57 -0300

    arriba todas

--- app/srcApirest/controllers/cuadroRochaSocket.js
+++ app/srcApirest/controllers/cuadroRochaSocket.js
@@ -25,58 +25,91 @@ module.exports = (io) => {
 	  })//on getRochas
 
 	  socket.on('sillas', (lista, callback) => {
+ 			console.log('llego')
 
-
-let insert = `INSERT INTO producto (CODIGO_PRODUCTO, DESCRIPCION, CATEGORIA, CODIGO_GENERICO, FAMILIA, TEMPORADA, 
+/*let insert = `INSERT INTO producto (CODIGO_PRODUCTO, DESCRIPCION, CATEGORIA, CODIGO_GENERICO, FAMILIA, TEMPORADA, 
 						GESTION, DIMENSION, RELACION, TIPO, TERMINO,
 						RUTA, RUTA1, RUTA2, CAD_2D, CAD_3D,
-						POSICION, CUERPO, FRENTE, CANTO, TRASCARA) VALUES ` 
+						POSICION, CUERPO, FRENTE, CANTO, TRASCARA) VALUES ` */
+
+let array = []
+let valA0 = ''
+let valA1 = ''
+let valR0 = ''
+let valR1 = ''
 
 		_.forEach(lista, (v, k) => {
-
-			insert += `('${v.cod}','${v.des}','${v.cat}','','generico','2',
+			valA0 = ''
+			valA1 = ''
+			valR0 = ''
+			valR1 = ''
+			/*insert += `('${v.cod}','${v.des}','${v.cat}','','generico','2',
 					   '${v.pais}','${v.proveedor}','${v.modelo}','${v.mecanismo}','${v.respaldo}',
-					   '${v.cod}_img.jpg','${v.cod}_img_1.jpg','${v.producto}','${v.cod}.dwg','${v.cod}.dwg',`
+					   '${v.cod}_img.jpg','${v.cod}_img_1.jpg','${v.producto}','${v.cod}.dwg','${v.cod}.dwg',`*/
 
+			//insert += ( Array.isArray(v.asiento) ) ? `'${v.asiento[0]}','${v.asiento[1]}',` : `'${v.asiento}','0',`
 
-			insert += ( Array.isArray(v.asiento) ) ? `'${v.asiento[0]}','${v.asiento[1]}',` : `'${v.asiento}','0',`
+			if (Array.isArray(v.asiento)) {
+				valA0 = v.asiento[0]
+				valA1 = v.asiento[1]
+			} else {
+				valA0 = v.asiento
+				valA1 = 0
+			}
 
-			insert += (Array.isArray(v.respaldocolor)) ? `'${v.respaldocolor[0]}','${v.respaldocolor[1]}',` : `'${v.respaldocolor}','0',`
+			//insert += (Array.isArray(v.respaldocolor)) ? `'${v.respaldocolor[0]}','${v.respaldocolor[1]}',` : `'${v.respaldocolor}','0',`
 
-			insert += `'${v.estructura}'),`
+			if (Array.isArray(v.respaldocolor)) {
+				valR0 = v.respaldocolor[0]
+				valR1 = v.respaldocolor[1]
+			} else {
+				valR0 = v.respaldocolor
+				valR1 = 0
+			}
 
+			//insert += `'${v.estructura}'),`
 
+			array.push( [ v.cod,v.des,v.cat,``,`generico`,`2`,
+					   	  v.pais,v.proveedor,v.modelo,v.mecanismo,v.respaldo,
+					   `${v.cod}_img.jpg`,`${v.cod}_img_1.jpg`,`${v.producto}`,`${v.cod}.dwg`,`${v.cod}.dwg`,
+					   valA0, valA1, valR0, valR1, v.estructura ] )
 
 
 			_.forEach(v.hijos, (vv, kk) => {
 
 
-				insert += `('${vv.cod}','${vv.des}','${vv.cat}','${v.cod}','silla','2',
+				/*insert += `('${vv.cod}','${vv.des}','${vv.cat}','${v.cod}','silla','2',
 						   '${v.pais}','${v.proveedor}','${v.modelo}','${v.mecanismo}','${v.respaldo}',
 						   '${vv.cod}_img.jpg','${vv.cod}_img_1.jpg','${v.producto}','${vv.cod}.dwg','${vv.cod}.dwg',
-						   '${vv.asiento}','0','${vv.respaldocolor}','0','${vv.estructura}'),`
+						   '${vv.asiento}','0','${vv.respaldocolor}','0','${vv.estructura}'),`*/
+
+			array.push( [ vv.cod,vv.des,vv.cat,v.cod,`silla`,`2`,
+						   v.pais,v.proveedor,v.modelo,v.mecanismo,v.respaldo,
+						   `${vv.cod}_img.jpg`,`${vv.cod}_img_1.jpg`,v.producto,`${vv.cod}.dwg`,`${vv.cod}.dwg`,
+						   vv.asiento,`0`,vv.respaldocolor,`0`,vv.estructura ] )
+
 
 			})
 
 		})
 
-insert = insert.substring(0, insert.length-1)
+//insert = insert.substring(0, insert.length-1)
 
-callback( insert )
 
-/*
+//callback( insert )
+
+
     pool.getConnection( (err, connection) => {
           connection.query(`INSERT INTO producto (CODIGO_PRODUCTO, DESCRIPCION, CATEGORIA, CODIGO_GENERICO, FAMILIA, TEMPORADA, 
-						GESTION, DIMENSION, RELACION, TIPO, TERMINO,
-						POSICION, CUERPO, FRENTE, CANTO, TRASCARA,
-						RUTA, RUTA1, RUTA2, CAD_2D, CAD_3D,
-						POSICION, CUERPO, FRENTE, CANTO, TRASCARA) VALUES ${insert}`, (err, rows, fields) => {
+							GESTION, DIMENSION, RELACION, TIPO, TERMINO,
+							RUTA, RUTA1, RUTA2, CAD_2D, CAD_3D,
+							POSICION, CUERPO, FRENTE, CANTO, TRASCARA) VALUES ?`, [array], (err, rows, fields) => {
             connection.release()
             if (err) console.log( err ) 
             callback( rows )
           })
     })
-*/
+
 
 	  })//on getRochas
 
@@ -97,4 +130,16 @@ callback( insert )
 	                reject( console.log('Error ' + err) )
 	          }) 
 	      })
+
+
+
+`INSERT INTO producto (CODIGO_PRODUCTO, DESCRIPCION, CATEGORIA, CODIGO_GENERICO, FAMILIA, TEMPORADA, 
+						GESTION, DIMENSION, RELACION, TIPO, TERMINO,
+						POSICION, CUERPO, FRENTE, CANTO, TRASCARA,
+						RUTA, RUTA1, RUTA2, CAD_2D, CAD_3D,
+						POSICION, CUERPO, FRENTE, CANTO, TRASCARA) VALUES ${insert}`
+
+
+
+
 */
